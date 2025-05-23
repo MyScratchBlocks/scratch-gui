5cd5856b5b8f924dfebd23baddfc38e80151e6f3
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-23 16:34:28 +0100

    Update project-fetcher-hoc.jsx

--- src/lib/project-fetcher-hoc.jsx
+++ src/lib/project-fetcher-hoc.jsx
@@ -198,8 +198,8 @@ const ProjectFetcherHOC = function (WrappedComponent) {
         vm: PropTypes.instanceOf(VM)
     };
     ProjectFetcherComponent.defaultProps = {
-        assetHost: 'https://assets.scratch.mit.edu',
-        projectHost: 'https://projects.scratch.mit.edu'
+        assetHost: 'https://Editor-Compiler.onrender.com/assets',
+        projectHost: 'https://Editor-Compiler.onrender.com/projects
     };
 
     const mapStateToProps = state => ({
