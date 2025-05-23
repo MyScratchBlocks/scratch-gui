8ad7a653299cc900108a5f4b187e14ba1a0edc82
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-23 17:10:39 +0100

    Update tw-project-meta-fetcher-hoc.jsx

--- src/lib/tw-project-meta-fetcher-hoc.jsx
+++ src/lib/tw-project-meta-fetcher-hoc.jsx
@@ -8,8 +8,8 @@ import {setAuthor, setDescription} from '../reducers/tw';
 
 export const fetchProjectMeta = async projectId => {
     const urls = [
-        `https://trampoline.turbowarp.org/api/projects/${projectId}`,
-        `https://trampoline.turbowarp.xyz/api/projects/${projectId}`
+        `https://Editor-Compiler.onrender.com/api/projects/${projectId}/meta`,
+        `https://Editor-Compiler.onrender.com/api/projects/${projectId}/meta`
     ];
     let firstError;
     for (const url of urls) {
