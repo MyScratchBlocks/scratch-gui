251afc6d065004ba32dd999dc39802320b26beda
MyScratchedAccount <benjmain801@icloud.com>
2025-05-24 14:33:07 +1000

    Update menu-bar.jsx

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -1041,7 +1041,7 @@ class MenuBar extends React.Component {
                             {/* todo: icon */}
                             <Button className={styles.feedbackButton}>
                                 <FormattedMessage
-                                    defaultMessage="Back to the {APP_NAME} website"
+                                    defaultMessage="{APP_NAME} Website"
                                     description="Button to go to the scratch mod's about page"
                                     id="tw.feedbackButton"
                                     values={{
