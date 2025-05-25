3e7ea6e2ce8dac92e9e7f16d8ddae1d32ad7c5c7
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-25 13:27:35 +0100

    Update tw-packager-integration-hoc.jsx

--- src/lib/tw-packager-integration-hoc.jsx
+++ src/lib/tw-packager-integration-hoc.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import log from './log';
 import {getIsShowingProject} from '../reducers/project-state';
 
-const PACKAGER_URL = 'https://myscratchblocks.github.io/packager;
+const PACKAGER_URL = 'https://myscratchblocks.github.io/packager';
 const PACKAGER_ORIGIN = PACKAGER_URL;
 
 const PackagerIntegrationHOC = function (WrappedComponent) {
