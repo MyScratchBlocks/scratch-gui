5deaacf6cdda9794b9ef11c7173a409001775f7e
MyScratchedAccount <benjmain801@icloud.com>
2025-05-25 11:20:44 +1000

    Update tw-username.js

--- src/lib/tw-username.js
+++ src/lib/tw-username.js
@@ -1,10 +1,17 @@
-const generateRandomUsername = () => {
-    const DIGITS = 4;
-    const randomNumber = Math.round(Math.random() * (10 ** DIGITS));
-    const randomId = randomNumber.toString().padStart(DIGITS, '0');
-    const randomUsername = `MyScratchBlocks-Coder-${randomId}`;
-    return randomUsername;
-};
+const username = localStorage.getItem('username');
+
+ const generateRandomUsername = () => {
+   if (username === "") {
+     const DIGITS = 4;
+     const randomNumber = Math.round(Math.random() * (10 ** DIGITS));
+     const randomId = randomNumber.toString().padStart(DIGITS, '0');
+     const randomUsername = `MyScratchBlocks-Coder-${randomId}`;
+     return randomUsername;
+    } else {
+       const randomUsername = `${username}`
+       return randomUsername;
+   }
+    
 
 export {
     generateRandomUsername
