45a3bbb4145c86ffe64a34ec319036d548a07a38
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-28 14:47:23 +0100

    Update author-info.jsx

--- src/components/menu-bar/author-info.jsx
+++ src/components/menu-bar/author-info.jsx
@@ -56,7 +56,7 @@ const AuthorInfo = ({projectId, ...props}) => (
     projectId ? (
         <a
             className={styles.link}
-            href={`https://scratch.mit.edu/projects/${projectId}`}
+            href={`https://myscratchblocks.github.io/projects#${projectId}`}
             target="_blank"
             rel="noreferrer"
         >
