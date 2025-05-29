7134085c5900f16f184dcf349cd81c402d52da5c
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-29 10:25:38 +0100

    Update tw-username.js

--- src/lib/tw-username.js
+++ src/lib/tw-username.js
@@ -1,19 +1,20 @@
-const username = localStorage.getItem('username');
-
- const generateRandomUsername = () => {
-   if (username === "") {
-     const DIGITS = 4;
-     const randomNumber = Math.round(Math.random() * (10 ** DIGITS));
-     const randomId = randomNumber.toString().padStart(DIGITS, '0');
-     const randomUsername = `MyScratchBlocks-Coder-${randomId}`;
-     return randomUsername;
-    } else {
-       const randomUsername = `${username}`
-       return randomUsername;
-   }
- }
+const generateRandomUsername = () => {
+  let username = localStorage.getItem('username');
+  
+  if (username && username !== "") {
+    return username;
+  } else {
+    const DIGITS = 4;
+    const randomNumber = Math.floor(Math.random() * (10 ** DIGITS));
+    const randomId = randomNumber.toString().padStart(DIGITS, '0');
+    const randomUsername = `MyScratchBlocks-${randomId}`;
     
+    localStorage.setItem('username', randomUsername);
+    
+    return randomUsername;
+  }
+};
 
 export {
-    generateRandomUsername
+  generateRandomUsername
 };
