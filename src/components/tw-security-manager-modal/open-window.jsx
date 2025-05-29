464d390eef3028eeeae44c63a85674d21eddea7e
MyScratchedAccount <benjmain801@icloud.com>
2025-05-29 16:38:44 +1000

    Update open-window.jsx

--- src/components/tw-security-manager-modal/open-window.jsx
+++ src/components/tw-security-manager-modal/open-window.jsx
@@ -15,7 +15,7 @@ const OpenWindowModal = props => (
         <p>
             <FormattedMessage
                 // eslint-disable-next-line max-len
-                defaultMessage="This website has not been reviewed by the {APP_NAME} developers. It may contain dangerous or malicious code."
+                defaultMessage="This website has not been reviewed by the {APP_NAME} developers. Possible risks include: Ads may show up out of nowhere or it may contain dangerous or malicious code."
                 description="Part of modal when a project attempts to open a window using an extension"
                 id="tw.openWindow.dangerous"
                 values={{
