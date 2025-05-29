3643485e655f0adde19dfe5238abaabdddc49998
MyScratchedAccount <benjmain801@icloud.com>
2025-05-29 17:10:49 +1000

    Update turbo-mode.jsx

--- src/components/turbo-mode/turbo-mode.jsx
+++ src/components/turbo-mode/turbo-mode.jsx
@@ -18,7 +18,7 @@ const TurboMode = ({isSmall}) => (
         {!isSmall && (
             <div className={styles.turboLabel}>
                 <FormattedMessage
-                    defaultMessage="Turbo Mode"
+                    defaultMessage="Turbo Mode is Enabled - Scripts run faster"
                     description="Label indicating turbo mode is active"
                     id="gui.turboMode.active"
                 />
