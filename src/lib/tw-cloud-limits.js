780c18bf8acade90054804c4373d0baea3dda108
MyScratchedAccount <benjmain801@icloud.com>
2025-06-07 15:56:29 +1000

    Update tw-cloud-limits.js

--- src/lib/tw-cloud-limits.js
+++ src/lib/tw-cloud-limits.js
@@ -1,5 +1,5 @@
 // The maximum number of cloudv ariables that can be created in this scratch-gui instance.
-export const MAXIMUM_CLOUD_VARIABLES = Infinity;
+export const MAXIMUM_CLOUD_VARIABLES = 250;
 
 // The maximum number of cloud variables that can be created in a vanilla Scratch.
 export const SCRATCH_MAX_CLOUD_VARIABLES = 10000;
