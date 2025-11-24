25f9300500746835bae523cc140923523f629cf7
6767676767 <192328467+kRxZykRxZy@users.noreply.github.com>
2025-11-24 16:53:21 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -38,15 +38,7 @@ const base = {
         compress: true,
         port: process.env.PORT || 8601,
         // allows ROUTING_STYLE=wildcard to work properly
-        historyApiFallback: {
-            rewrites: [
-                {from: /^\/\d+\/?$/, to: '/index.html'},
-                {from: /^\/\d+\/fullscreen\/?$/, to: '/fullscreen.html'},
-                {from: /^\/\d+\/editor\/?$/, to: '/editor.html'},
-                {from: /^\/\d+\/embed\/?$/, to: '/embed.html'},
-                {from: /^\/addons\/?$/, to: '/addons.html'}
-            ]
-        }
+        historyApiFallback: true
     },
     output: {
         library: 'GUI',
